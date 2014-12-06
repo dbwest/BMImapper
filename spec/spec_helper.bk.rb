74ce10dffbc091e6c7b06734e41bc451244dc271
David West <david.b.west@gmail.com>
2014-12-06 15:58:47 -0500

    spec_helper.rb

---
+++ spec/spec_helper.bk.rb
@@ -0,0 +1,24 @@
+require_relative '../lib/hiw_forklift'
+# For Ruby < 1.9.3, use this instead of require_relative
+# require(File.expand_path('../../lib/dish', __FILE__))
+
+#dependencies
+require 'minitest/autorun'
+require 'webmock/minitest'
+require 'vcr'
+require 'turn'
+
+Turn.config do |c|
+  # :outline  - turn's original case/test outline mode [default]
+  c.format  = :outline
+  # turn on invoke/execute tracing, enable full backtrace
+  c.trace   = true
+  # use humanized test names (works only with :outline format)
+  c.natural = true
+end
+
+#VCR config
+VCR.config do |c|
+  c.cassette_library_dir = 'spec/fixtures/hiw_forklift_cassettes'
+  c.stub_with :webmock
+end
